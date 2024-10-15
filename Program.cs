0bd0ca33ef6a772c2da2003781a4ca5f3f66b177
BolNick <BolNick09RUS@gmail.com>
2024-10-15 20:30:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ThreadsDZ1
+{
+    internal class Program
+    {
+        private static int count = 0;
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[4];
+            for (int i = 0; i < threads.Length; i++)   
+            {
+                threads[i] = new Thread(IncrementCount);
+                threads[i].Start();
+            }
+            for (int i = 0; i < threads.Length; i++)
+                threads[i].Join();
+
+            Console.WriteLine(count);
+        }
+
+
+        private static void IncrementCount()
+        {
+            for (int i = 0; i < 100000; i++)
+                count++;
+        }
+    }
+}
